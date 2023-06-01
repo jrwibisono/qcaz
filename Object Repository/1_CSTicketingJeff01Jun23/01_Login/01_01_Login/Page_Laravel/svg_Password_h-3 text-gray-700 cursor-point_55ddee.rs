70fbb8166109b36cb68ee39968f4af63a2c96349
jrwibisono <qc.azlogistik@gmail.com>
2023-06-01 21:09:10 +0700

    Penambahan Automate - CS Ticketing - Approval

---
+++ Object Repository/1_CSTicketingJeff01Jun23/01_Login/01_01_Login/Page_Laravel/svg_Password_h-3 text-gray-700 cursor-point_55ddee.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>svg_Password_h-3 text-gray-700 cursor-point_55ddee</name>
+   <tag></tag>
+   <elementGuidId>ceeafd1c-2fe0-4534-8a17-03472ee74925</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/following::*[name()='svg'][2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>svg.h-3.text-gray-700.cursor-pointer.block</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>svg</value>
+      <webElementGuid>8159e878-9fd7-4e67-b948-cc513f8048ae</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>h-3 text-gray-700 cursor-pointer block</value>
+      <webElementGuid>31a210bb-a798-4129-9c97-bb03974de8da</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>fill</name>
+      <type>Main</type>
+      <value>none</value>
+      <webElementGuid>b3c317c7-d9b4-4fdb-9038-089c372acb64</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>@click</name>
+      <type>Main</type>
+      <value>show = !show</value>
+      <webElementGuid>0debe5e3-0448-4d15-88f7-30e6cd32cf6f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>:class</name>
+      <type>Main</type>
+      <value>{ 'block': show, 'hidden': !show }</value>
+      <webElementGuid>eef36746-fa0a-485b-b2b8-ebb3fce29b75</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xmlns</name>
+      <type>Main</type>
+      <value>http://www.w3.org/2000/svg</value>
+      <webElementGuid>82c2bc0c-4518-4e6d-8a86-58c0422b89d6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>viewBox</name>
+      <type>Main</type>
+      <value>0 0 640 512</value>
+      <webElementGuid>80af6eac-466d-40b5-8737-77cc7fe94d02</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;font-sans text-gray-900 antialiased&quot;]/div[@class=&quot;min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100&quot;]/div[@class=&quot;w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg&quot;]/form[1]/div[@class=&quot;mb-6&quot;]/div[@class=&quot;relative&quot;]/div[@class=&quot;absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5&quot;]/svg[@class=&quot;h-3 text-gray-700 cursor-pointer block&quot;]</value>
+      <webElementGuid>3ac50c84-f6fd-4476-8602-239d88698313</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/following::*[name()='svg'][2]</value>
+      <webElementGuid>845af604-5d2a-4ac9-8fd4-04b2259c1c0e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Email'])[1]/following::*[name()='svg'][2]</value>
+      <webElementGuid>3dabcb0a-fc3b-4672-93cc-aa2c08d40d75</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Log in'])[1]/preceding::*[name()='svg'][1]</value>
+      <webElementGuid>18c3273f-2398-4d2e-9b6b-22e1f16067d0</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
