b0e65c4ba40ab96527dd41de0647294d7d5bcbb3
qc3Azlogistik <qc3.azlogistik@gmail.com>
2023-10-22 19:41:25 +0700

    Update Dandy 221023 1941: Update Cek user 0 Buyer dan Percobaan Kirim hasil ke WA

---
+++ Object Repository/0_MuatmuatJeff23Apr23/04_Buyer/B01_TransportationStore/B01_TS01_KendaraanKargo/Page_Transportation Store  muatmuat.com/div_first_card.rs
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_first_card</name>
+   <tag></tag>
+   <elementGuidId>a74884e7-cd9b-47b2-a91e-5d42bd10b17c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.card-body.card-landing</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[5]/div[3]/div/div[6]/div[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>9f748afa-eb74-4f63-9572-14df64228df3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-body card-landing</value>
+      <webElementGuid>220b1ec4-d0b5-4837-ad40-fdd90c9d2df6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        
+                            
+                            
+                                
+                                    
+                                        
+                                            
+                                        
+                                        Verified
+                                    
+                                
+                            
+                            
+                            
+                        
+                        kargo 
+                        
+                            
+                            Rp 11.111.111.111
+                        
+                        
+                            
+                                 Tahun Kendaraan : 
+                                1900 
+                            
+                            
+                                 Kondisi : 
+                                Baru 
+                            
+                        
+                        
+                        
+                            
+                                
+                                    
+                                
+                                 Kec. Seputih Surabaya, Kab. Lampung Tengah 
+                            
+                            3 hari yang lalu 
+                        
+                    </value>
+      <webElementGuid>918f2592-8c60-4290-9a78-c5c90bf1ae89</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;grid-card-api&quot;)/div[@class=&quot;card card-product swiper-slide&quot;]/div[@class=&quot;card-body card-landing&quot;]</value>
+      <webElementGuid>cd1e8f9f-2295-4e77-87cb-d56f3f10744c</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='grid-card-api']/div/div</value>
+      <webElementGuid>5924daa3-0d22-452f-8b0e-532eb67f6277</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hasil Tidak Ditemukan!'])[2]/following::div[3]</value>
+      <webElementGuid>a49285da-7452-498e-bb91-3a0059ef9c43</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hasil Tidak Ditemukan!'])[1]/following::div[4]</value>
+      <webElementGuid>c98d1c66-6044-4a6f-b200-c21ecef4e12c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[6]/div/div</value>
+      <webElementGuid>96b5427e-413e-4e17-8b4d-093c8426f87f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+                        
+                            
+                            
+                                
+                                    
+                                        
+                                            
+                                        
+                                        Verified
+                                    
+                                
+                            
+                            
+                            
+                        
+                        kargo 
+                        
+                            
+                            Rp 11.111.111.111
+                        
+                        
+                            
+                                 Tahun Kendaraan : 
+                                1900 
+                            
+                            
+                                 Kondisi : 
+                                Baru 
+                            
+                        
+                        
+                        
+                            
+                                
+                                    
+                                
+                                 Kec. Seputih Surabaya, Kab. Lampung Tengah 
+                            
+                            3 hari yang lalu 
+                        
+                    ' or . = '
+                        
+                            
+                            
+                                
+                                    
+                                        
+                                            
+                                        
+                                        Verified
+                                    
+                                
+                            
+                            
+                            
+                        
+                        kargo 
+                        
+                            
+                            Rp 11.111.111.111
+                        
+                        
+                            
+                                 Tahun Kendaraan : 
+                                1900 
+                            
+                            
+                                 Kondisi : 
+                                Baru 
+                            
+                        
+                        
+                        
+                            
+                                
+                                    
+                                
+                                 Kec. Seputih Surabaya, Kab. Lampung Tengah 
+                            
+                            3 hari yang lalu 
+                        
+                    ')]</value>
+      <webElementGuid>70b71995-e36e-4c9f-b821-ffc340549699</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/0_MuatmuatJeff23Apr23/04_Buyer/B01_TransportationStore/B01_TS01_KendaraanKargo/Page_Transportation Store  muatmuat.com/span_text_ceklogin.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_text_ceklogin</name>
+   <tag></tag>
+   <elementGuidId>35128560-7c6e-41aa-a2d8-2ed6186a3015</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[9]/div[1]/div[2]/div[2]/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
